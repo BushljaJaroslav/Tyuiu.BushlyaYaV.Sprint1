ceb324e61b7261e3805d13497e12970232d0d7a8
Tyuiu.BushljaJV@outlook.com <Tyuiu.BushljaJV@outlook.com>
2024-09-25 13:12:35 +0500

    Task0

--- Tyuiu.BushlyaYaV.Sprint1.Task0.V30/Program.cs
+++ Tyuiu.BushlyaYaV.Sprint1.Task0.V30/Program.cs
@@ -10,36 +10,25 @@ namespace Tyuiu.BushlyaYaV.Sprint1.Task0.V30
             Console.Title = "Спринт 1 | Выполнил: Бушля Я. В. | ИСНТб-24-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
-            Console.WriteLine("* Тема: Организация ввода\\вывода                                         *");
-            Console.WriteLine("* в консольных приложениях                                                *");
-            Console.WriteLine("* Задание #1                                                              *");
-            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
             Console.WriteLine("* Выполнил: Бушля Ярослав Владимирович | ИСТНб-24-1                       *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
-            Console.WriteLine("* вычисляет результат по формуле (x+y)/(3*y) и печатает его на экране.    *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 20*5-4                  *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-
-            double x, y;
-            
-            Console.WriteLine("Введите значение X:"));
-            x = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Введите значение Y:"));
-            y = Convert.ToDouble(Console.ReadLine());
-
+            Console.WriteLine("*  20 * 5 - 4                                                             *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine(ds.Calculate(x, y));
-
+            Console.WriteLine(ds.Calculate());
             Console.ReadLine();
-
 
         }
     }

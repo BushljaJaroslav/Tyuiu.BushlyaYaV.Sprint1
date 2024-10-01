0236feb6f631834b0d5c87b40056d79a59ce6682
Tyuiu.BushljaJV@outlook.com <Tyuiu.BushljaJV@outlook.com>
2024-10-01 21:25:07 +0500

    Task4

---
+++ Tyuiu.BushlyaYaV.Sprint1.Task4.V16/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+using Tyuiu.BushlyaYaV.Sprint1.Task4.V16.Lib;
+// Написать программу, которая запрашивает у пользователя исходные данные,
+//вычисляет результат по формуле и печатает его на экране. Ответ до 3 знаков после запятой
+//Формула  1 / x + 4
+
+
+
+
+
+namespace Tyuiu.BushlyaYaV.Sprint1.Task4.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Бушля Я. В. | ИСТНб-24-1 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Бушля Ярослав Владимирович | ИСТНб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя                  *");
+            Console.WriteLine("* исходные данные, вычисляет результат по формуле и печатает              *");
+            Console.WriteLine("* и печатает его на экране. Ответ округлите до 3 знаков после запятой.    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("1 / (x + 4) = " + ds.Calculate(x));
+
+            Console.ReadKey();
+        }
+    }
+}

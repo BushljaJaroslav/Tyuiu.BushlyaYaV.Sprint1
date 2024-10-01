551989b4b0e7cd499ab9eb87ee687c1b5aa76cd2
Tyuiu.BushljaJV@outlook.com <Tyuiu.BushljaJV@outlook.com>
2024-10-01 23:54:39 +0500

    Task7

---
+++ Tyuiu.BushlyaYaV.Sprint1.Task7.V10.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+
+using Tyuiu.BushlyaYaV.Sprint1.Task7.V10.Lib;
+namespace Tyuiu.BushlyaYaV.Sprint1.Task7.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            var res = ds.Calculate(x);
+            var wait = -13.142;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
